eac9b51667e1b2a37f36f0f4f1c49984bcd8d2c5
dev-EstherKubania <eskury@gmail.com>
2023-08-28 12:45:03 -0700

    updates

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             divide = 0
             print("wrong type")
-        except ZeroDivisionError:
+        except (ZeroDivisionError, ValueError):
             divide = 0
             print("division by 0")
         except IndexError:
