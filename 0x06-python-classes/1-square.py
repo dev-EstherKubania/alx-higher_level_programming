224c789994ea84319c42efb79af45805803c7711
dev-EstherKubania <eskury@gmail.com>
2023-08-29 13:55:39 -0700

    updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-Module contains: class Square
-class Square defines a square by: (based on 0-square.py)
+module that Write a class
+Square that defines a square based on 0-square.py
 """
 class Square:
-    """
-        Square: This class represents a square.
-        Attributes:
-        __size (no type): The size of the square.
-        Method:
-            __init__ : Initializes a new Square instance.
-    """
-    def __init__(self, size)
-        """
-        Initializes a new Square instance.
-        Args:
-            size (no type): The size of the square.
-        """
+    """class that defines a square"""
+    def __init__(self, size):
         self.__size = size
+        """
+        Private instance attribute: size
+        Instantiation with size
+        (no type/value verification)
+        """
