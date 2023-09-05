156b50ec2bd1a08d438521b9ed36cbaebe852fdc
dev-EstherKubania <eskury@gmail.com>
2023-09-04 22:23:22 -0700

    divide matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Divide Matrix """
+
+
+def matrix_divided(matrix, div):
+    """
+    Divide all elements of a matrix by a number.
+
+    Args:
+        matrix (list of lists): The matrix to be divided.
+        div (int or float): The divisor.
+
+    Returns:
+        list of lists: A new matrix with elements divided by div, rounded to 2 decimal places.
+
+    Raises:
+        TypeError: If the matrix is not a list of lists of integers or floats.
+        TypeError: If each row of the matrix doesn't have the same size.
+        TypeError: If div is not a number (integer or float).
+        ZeroDivisionError: If div is equal to 0.
+    """
+    # Check if matrix is a list of lists
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # Check if each row has the same size
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    # Check if div is a number
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    # Check if div is not equal to 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Divide each element by div and round to 2 decimal places
+    result = [[round(element / div, 2) for element in row] for row in matrix]
+
+    return result
