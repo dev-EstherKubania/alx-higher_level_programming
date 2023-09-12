4f37601da32e65a74aa11c3de3bf4cf511087910
dev-EstherKubania <eskury@gmail.com>
2023-09-12 08:19:00 -0700

    error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,8 @@ Module: 1-write_file
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
+    Writes a string to a text file (UTF8) and returns the
+    number of characters written.
     Args:
         filename (str): The name of the file to be written.
         text (str): The string to be written to the file.
