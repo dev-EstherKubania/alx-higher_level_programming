598e3b629229a745e7cfd35ddcf00ef0bf930211
dev-EstherKubania <eskury@gmail.com>
2023-09-12 10:43:42 -0700

    a class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,10 +5,13 @@
 
 
 class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a Student instance with a first name, last name, and age.
-
+        Initializes a Student instance with first_name, last_name, and age.
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -22,12 +25,12 @@ class Student:
         """
         Retrieves a dictionary representation of a Student instance.
         Args:
-            attrs (list): A list of attribute names to retrieve. If None,
-            retrieve all attributes.
+            attrs (list): A list of strings representing attribute names.
         Returns:
-            dict: A dictionary representation of the Student instance.
+            dict: A dictionary containing specified attributes of the Student.
         """
-        if attrs and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if /
-                    hasattr(self, attr)}
+        if attrs is not None and isinstance(attrs, list) and /
+        all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs /
+                    if hasattr(self, attr)}
         return self.__dict__
