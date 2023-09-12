d8343766e4d48bad0883630497ad5953a44da967
dev-EstherKubania <eskury@gmail.com>
2023-09-12 07:11:58 -0700

    pycode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,16 +3,14 @@
 Module: 0-read_file
 """
 
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
-
     Args:
         filename (str): The name of the file to be read.
-
     Note:
         If no filename is provided, an empty string is assumed.
-
     Returns:
         None
     """
