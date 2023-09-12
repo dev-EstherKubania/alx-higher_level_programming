0d5a68e7b917ce0677554f90c3b7cb78be0c3a8a
dev-EstherKubania <eskury@gmail.com>
2023-09-12 10:00:11 -0700

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,8 @@ A python script that adds all arguments to a Python List.
 """
 
 import sys
-from load_from_json_file import load_from_json_file
-from save_to_json_file import save_to_json_file
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
 
