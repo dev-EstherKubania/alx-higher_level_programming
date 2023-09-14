98c6fde2ba0ff6e3e0f3b273d548eadada534992
dev-EstherKubania <eskury@gmail.com>
2023-09-14 14:20:03 -0700

    the first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    contains a class Base.
+"""
+
+
+class Base:
+    """
+    Base class for managing unique IDs.
+    Attributes:
+        __nb_objects (int): Private class attribute to keep track of the number of objects.
+        id (int): Public instance attribute representing the object's ID.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base instance.
+        Args:
+            id (int, optional): The ID for the instance. If not provided, a unique ID will be generated.
+        Returns:
+            None
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
