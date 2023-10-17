d461b430208ff0a4b9e1558522bec824e4653d85
dev-EstherKubania <eskury@gmail.com>
2023-10-17 02:57:05 -0700

    create table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Task: Create first_table in the current database
+
+-- Check if the table exists, if not, create it
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
