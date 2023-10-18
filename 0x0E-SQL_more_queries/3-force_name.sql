bbf41b5885e268bdb11cc15e7c2636a8c8f640d2
dev-EstherKubania <eskury@gmail.com>
2023-10-18 05:53:04 -0700

    create a table forcename

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,9 @@
+-- Create table force_name
+
+USE your_database_name; -- Replace with your actual database name
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
+
