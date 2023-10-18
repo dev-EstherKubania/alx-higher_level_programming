c1c3cb8711f7ee28ce081e517f1b69683d615291
dev-EstherKubania <eskury@gmail.com>
2023-10-18 05:56:07 -0700

    updates

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- List privileges for users user_0d_1 and user_0d_2
 
-SELECT * FROM mysql.user
-WHERE user = 'user_0d_1' OR user = 'user_0d_2';
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
