a11911241ebd22524c06ac4ad4c53411eea831a6
dev-EstherKubania <eskury@gmail.com>
2023-10-18 07:24:55 -0700

    update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,11 +5,4 @@
 -- Group the results by genre id
 -- Order the results by the number of shows linked in descending order, and genre id in ascending order
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-GROUP BY tv_genres.id
-ORDER BY number_of_shows DESC, tv_genres.id ASC;
-
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name;
