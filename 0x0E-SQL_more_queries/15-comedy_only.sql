ef5127bc356e5fca47851a9fd59ec558cb1fc30b
dev-EstherKubania <eskury@gmail.com>
2023-10-18 07:28:39 -0700

    list all comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- Switch to the hbtn_0d_tvshows database
+-- Select the titles of Comedy shows
+-- Use INNER JOINs to link genres, show genres, and shows
+-- Filter the results to only include shows with the genre "Comedy"
+-- Order the results by the show title in ascending order
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title;
