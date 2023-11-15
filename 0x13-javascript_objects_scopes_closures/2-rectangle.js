50c7fad765b16393c3a5165513ea63c448af2380
dev-EstherKubania <eskury@gmail.com>
2023-11-15 14:56:15 -0800

    Create an instance method called double() that multiples the width and the height of the rectangle by 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is not a positive integer, create an empty object
-      return {};
+
+// script that creates a class.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
-}
-
-module.exports = Rectangle;
+};
