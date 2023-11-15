000ef57cd6cdeb1b3543e27f2496a9b9405dcbec
dev-EstherKubania <eskury@gmail.com>
2023-11-15 14:43:05 -0800

    a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is not a positive integer, create an empty object
+      return {};
+    }
+
     this.width = w;
     this.height = h;
   }
