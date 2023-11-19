b07c08e9c1abc94acd4f17d3d0691159a72c2822
dev-EstherKubania <eskury@gmail.com>
2023-11-19 15:10:39 -0800

    uses MySQLdb to filter and display values from the states table based on the provided state name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,20 +6,18 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY
-                   '{}' ORDER BY id ASC")
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(state_name)
+    cursor.execute(query)
 
     rows = cursor.fetchall()
 
