a904fb898ac0fd9f71c8c7944cc11155a4466931
dev-EstherKubania <eskury@gmail.com>
2023-11-19 15:21:22 -0800

    updates

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,13 +14,14 @@ if __name__ == "__main__":
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
     cursor = db.cursor()
 
     query = """SELECT cities.id, cities.name, states.name
                FROM cities
                JOIN states ON cities.state_id = states.id
-               ORDER BY cities.id"""
+               ORDER BY cities.id ASC"""
 
     cursor.execute(query)
 
