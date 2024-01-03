8a0306f9e2e99c880487b85c126cd09b43009c7f
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-01-03 20:58:56 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,9 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
+    return console.log(err);
   }
+  process.stdout.write(data);
 });
